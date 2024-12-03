cb7aa6c69e54f00b262b1220f2de740aec7743d5
mo ismail <141470288+ismail8445@users.noreply.github.com>
2024-12-03 19:29:21 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,31 @@
+/// If else condition
+// const balance = 1500
+
+// if (balance < 900) {
+//     console.log("balance is than 900")
+// } else if (balance < 950) {
+//     console.log("balance is less than 950")
+// } else if (balance < 1200) {
+//     console.log("balance is less than 1200")
+// } else {
+//     console.log("balance is greater than 1200")
+// }
+
+////Multiple AND condition check
+//const userLoggin = true
+// const debitCard = true
+
+// if (userLoggin && debitCard ) {
+//     console.log("user can login and use debit card")
+// }
+
+
+
+//Multiple or condotion check
+
+const userLogginFromGoogle = true
+const userLogginFromEmail = false
+
+if (userLogginFromEmail || userLogginFromGoogle) {
+    console.log("user can login from google or email")
+}
